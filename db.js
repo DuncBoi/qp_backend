522b03d5684dd97d6667fef5aae931373ec5742e
duncangreene <duncan.greene@columbia.edu>
2025-03-15 19:05:51 -0700

    fixes

--- db.js
+++ db.js
@@ -7,7 +7,19 @@ const pool = new Pool({
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
+
+pool.connect()
+    .then(client => {
+        console.log('✅ Successfully connected to PostgreSQL!')
+        client.release() // Release connection back to pool
+    })
+    .catch(err => console.error('❌ Database connection error:', err))
+
+
 
 module.exports = pool
